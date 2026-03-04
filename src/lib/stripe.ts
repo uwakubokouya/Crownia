0e7f4ed49727344983fa2a413d6a1690f03d2675
guang <guangzaishangwa@gmail.com>
2026-03-04 13:46:41 +0900

    fix: stripe build error on vercel

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy', {
     apiVersion: '2026-02-25.clover', // Required type version
     appInfo: {
         name: 'Crownia',
